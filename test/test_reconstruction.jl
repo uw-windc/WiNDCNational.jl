35485088ae8b150dfd202f649fb5c81ee9f9b486
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-10-17 06:41:42 -0500

    Add dataframes to test

--- test/test_reconstruction.jl
+++ test/test_reconstruction.jl
@@ -1,5 +1,6 @@
-@testitem "Calibration - Ensure Balance" begin
+@testitem "Reconstruction - Ensure Balance is Maintained" begin
     using MPSGE
+    using DataFrames
     import PATHSolver
     PATHSolver.c_api_License_SetString("2830898829&Courtesy&&&USR&45321&5_1_2021&1000&PATH&GEN&31_12_2025&0_0_0&6000&0_0")
 
