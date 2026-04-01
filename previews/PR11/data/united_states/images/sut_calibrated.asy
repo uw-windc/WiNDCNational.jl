e56c082c3af011db466776acbe17dbfae6ce56a2
Documenter.jl <documenter@juliadocs.github.io>
2026-04-01 15:02:57 +0000

    build based on 85f1e8e

---
+++ previews/PR11/data/united_states/images/sut_calibrated.asy
@@ -0,0 +1,92 @@
+import settings;
+//settings.prc = false;
+settings.outformat="png";
+settings.render = 16;
+
+size(500,500);
+
+
+
+
+real width = 8;
+real table_width = 100;
+
+path value_added = (0,-width)--(0,-7*width)--(table_width,-7*width)--(table_width,-width)--cycle;
+
+draw(value_added);
+
+label("Labor\_Demand", (50, -1.75*width));
+draw((0, -2.5*width)--(100,-2.5*width), dashed);
+  
+label("Capital\_Demand", (50, -(4+2.5)/2*width));
+draw((0, -4*width)--(100,-4*width), dashed);
+
+label("Sector\_Subsidy", (50, -(4+5.5)/2*width));
+draw((0, -5.5*width)--(100,-5.5*width), dashed);
+
+//label("Value Added", (50,-4*width/2));
+label("Other\_Tax", (50,-(5.5+7)/2*width));
+
+
+
+path intermediate_demand = (0,0) -- (0,10*width) -- (table_width,10*width) -- (table_width,0) -- cycle;
+
+label(rotate(90)*"commoditity", (0, 5*width), align = W);
+label("sector", (table_width/2, 10*width), align = N);
+
+
+draw(intermediate_demand);
+label("Intermediate\_Demand*", (50,65));
+
+real start = table_width + width;
+table_width = 5.5*width;
+
+path final_demand = (start, 0) -- (start, 10*width) -- (start + table_width, 10*width) -- (start + table_width, 0) -- cycle;
+draw(final_demand);
+
+label(rotate(90)*"Investment\_Final\_Demand", (start + table_width/4 - .5*width, 5*width), fontsize(8));
+
+draw((start + table_width/4, 0)--(start + table_width/4, 10*width), dashed);
+label(rotate(90)*"Government\_Final\_Demand", (start + 2*table_width/4 - .5*width, 5*width), fontsize(8));
+
+
+draw((start + 2*table_width/4, 0)--(start + 2*table_width/4, 10*width), dashed);
+label(rotate(90)*"Personal\_Consumption*", (start + 3*table_width/4 - .5*width, 5*width), fontsize(8));
+
+
+draw((start + 3*table_width/4, 0)--(start + 3*table_width/4, 10*width), dashed);
+label(rotate(90)*"Export", (start + 4*table_width/4 - .5*width, 5*width));
+
+label("Use", ((start+table_width)/2, 12*width), fontsize(20));
+
+
+
+real start = start + table_width + 5*width;
+table_width = 100;
+
+label("Supply", ((start + start+table_width+width + 1.5*6*width)/2, 12*width), fontsize(20));
+
+label(rotate(90)*"commoditity", (start, 5*width), align = W);
+label("sector", (start + table_width/2, 10*width), align = N);
+
+path intermediate_supply = (start, 0) -- (start, 10*width) -- (start + table_width, 10*width) -- (start + table_width, 0) -- cycle;
+draw(intermediate_supply);
+
+label("Intermediate\_Supply*", (start + table_width/2, 65));
+
+real start = start + table_width + width;
+table_width = 1.5*7*width;
+
+draw((start, 0)--(start, 10*width)--(start + table_width, 10*width)--(start + table_width, 0)--cycle);
+
+for(int i = 1; i < 7; ++i) {
+  draw((start + 1.5*i*width, 0)--(start + 1.5*i*width, 10*width), dashed);
+}
+
+string[] labels = {"Household\_Supply*","Import*", "Margin\_Demand*", "Margin\_Supply*", "Duty", "Tax", "Subsidy"};
+
+for(string p : labels) {
+  label(rotate(90)*p, (start + 1.5*width/2, 5*width));
+  start = start + 1.5*width;
+}
+
---
+++ previews/PR11/data/united_states/sets.jl
@@ -0,0 +1,90 @@
+# # Sets and Parameters
+
+# WiNDCNational is built using `WiNDCContainer`, which provides a flexible way 
+# to define and manage sets and parameters within the data model. We will provide 
+# a listing of the sets used and demonstrate how to access and view them in code.
+
+# The sets and parameters are the same for both the `summary` and `detailed` 
+# tables. For this reason we will use the `summary` table as the basis for our
+# examples.
+
+using WiNDCNational 
+using DataFrames
+summary = build_us_table(:summary); 
+
+# ## Sets
+
+# To view all the sets, use the `sets` function:
+
+sets(summary) |> x-> sort(x, [:domain, :name])
+
+# The `|> x -> sort(x, :[:domain, :name])` sorts the sets by their domain, or the column
+# in the data table where the set elements are located, and their name. 
+
+#  By convention we use `snake_case` for sets that do not have the `parameter` domain.
+
+# Let's consider the set `margin` with domain `col`. I would like to view the 
+# elements:
+
+elements(summary, :margin)
+
+# The values in the `name` column are the elements of the set, they are 
+# NAICS codes as they appear in the BEA input/output tables. To examine the actual
+# data use:
+
+table(summary, :margin) |> x -> first(x, 5)
+
+# This will show the first 5 rows of the data table restricted to the  `margin` 
+# set. Recall `margin` had domain `col` and that is the column where the elements 
+# live. To demonstrate the multiple values in `col`, just view the unique values:
+
+table(summary, :margin) |> x -> unique(x, :col)
+
+# Because we are only viewing the first few rows, we only see the `Trans` code. 
+# We can further filter the data by specifying a particular element:
+
+table(summary, :margin => :Trade) |> x -> first(x, 5)
+
+# Again, we can extract the unique values:
+
+table(summary, :margin => :Trade) |> x -> unique(x, :col)
+
+# ## Parameters
+
+# Parameters are just sets with domain `:parameter`. 
+
+sets(summary) |> x-> subset(x, :domain => ByRow(==(:parameter)))
+
+# By convention, parameter names are capital-version of `snake_case`. Sets are 
+# required to have distinct names and this avoid conflicts. 
+
+# Extracting parameters is identical to extracting sets.
+
+table(summary, :Intermediate_Demand) |> x -> first(x, 5)
+
+# We can extract multiple parameters:
+
+table(summary, :Intermediate_Demand, :Intermediate_Supply) |> x -> first(x, 5)
+
+# Some parameters are composites of multiple parameters:
+
+elements(summary, :Value_Added)
+
+
+# ## Composite Parameters
+
+# Some parameters require aggregations and can't be extracted directly. Here
+# is a full list of these parameters:
+#
+# - [`zero_profit`](@ref)
+# - [`market_clearance`](@ref)
+# - [`margin_balance`](@ref)
+# - [`gross_output`](@ref)
+# - [`armington_supply`](@ref)
+# - [`output_tax`](@ref)
+# - [`sectoral_output`](@ref)
+# - [`output_tax_rate`](@ref)
+# - [`absorption_tax`](@ref)
+# - [`absorption_tax_rate`](@ref)
+# - [`import_tariff_rate`](@ref)
+# - [`balance_of_payments`](@ref)
